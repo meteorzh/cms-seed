9e87a30d771cc5c1e32e8a88c6cbd3a23da6db10
wenzhen <wenzhen@zhiyuandian.com>
2020-04-08 10:35:19 +0800

    Signed-off-by: wenzhen <wenzhen@zhiyuandian.com>

--- cms-seed-developer/cms-seed-developer-ui/src/app/pages/test/test.component.ts
+++ cms-seed-developer/cms-seed-developer-ui/src/app/pages/test/test.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { data } from "./test.data";
+import { TestService } from './test.service';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd';
+import { CommonObserver } from 'src/app/common/base-http.service';
 
 @Component({
     selector: 'test',
@@ -10,7 +14,9 @@ export class TestComponent implements OnInit {
 
     data: any = data;
 
-    constructor() { }
+    testResult: any;
+
+    constructor(private testService: TestService, private router: Router, private messageService: NzMessageService) { }
 
     ngOnInit(): void { }
 
@@ -21,5 +27,11 @@ export class TestComponent implements OnInit {
 
     getLast(children: any[]): any {
         return children[children.length - 1];
+    }
+
+    test(event: any): void {
+        this.testService.test().subscribe(new CommonObserver(this.router, this.messageService, resp => {
+            this.testResult = resp.data
+        }, true));
     }
 }
--- cms-seed-developer/cms-seed-developer-ui/src/app/pages/test/test.module.ts
+++ cms-seed-developer/cms-seed-developer-ui/src/app/pages/test/test.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestComponent } from './test.component';
 import { TestRoutingModule } from './test-routing.module';
+import { TestService } from './test.service';
+import { NzButtonModule } from 'ng-zorro-antd';
 
 @NgModule({
-    imports: [ CommonModule, TestRoutingModule ],
+    imports: [ CommonModule, TestRoutingModule, NzButtonModule ],
     declarations: [ TestComponent ],
-    exports: [ TestComponent ]
+    exports: [ TestComponent ],
+    providers: [ TestService ]
 })
 export class TestModule { }
